1d5b4ad41e557b491478e7b62cb7d130efd4f037
Your Name <kotibah99>
2022-11-15 01:51:16 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*typing*/
+var typed = new Typed(".typing",{
+    strings:[,"","full stack engineer","web Designer","web Developer","systems analyst"],
+    typeSpeed:150,
+    BackSpeed:150,
+    loop:true
+
+
+}) 
